f3a1c364ed7f04e87834975a77d86e5e0bd0d992
“Jayce” <15332019087@163.com>
2023-03-21 20:59:22 +0800

    初始化项目

---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <img slot="item-icon" src="../assets/img/tabBar/icon01.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../assets/img/tabBar/icon01_active.png"
+        alt=""
+      />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="../assets/img/tabBar/icon02.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../assets/img/tabBar/icon02_active.png"
+        alt=""
+      />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="../assets/img/tabBar/icon03.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../assets/img/tabBar/icon03_active.png"
+        alt=""
+      />
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="../assets/img/tabBar/icon04.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../assets/img/tabBar/icon04_active.png"
+        alt=""
+      />
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "../components/tabBar/TabBar";
+import TabBarItem from "../components/tabBar/TabBarItem.vue";
+
+export default {
+  name: "MainTabBar",
+  components: { TabBar, TabBarItem },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/tabBar/TabBarItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+    <!-- <img name="item-icon" src="./assets/img/tabBar/icon01.png" alt="" /> -->
+    <!-- <div name="item-text">首页</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: "orange",
+    },
+  },
+  data() {
+    return {
+      // isActive: true,
+    };
+  },
+  computed: {
+    isActive() {
+      // 当前活跃路由的路径与this.path路径是否相等
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    },
+  },
+  methods: {
+    itemClick() {
+      this.$router.push(this.path);
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  /* display: flex;
+  justify-content: center; */
+  color: #ccc;
+}
+.tab-bar-item img {
+  height: 24px;
+  width: 24px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+/* .active {
+  color: red;
+} */
+</style>
