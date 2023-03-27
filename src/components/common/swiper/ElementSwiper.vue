beff11ceef06f9d0c85398b542180ecd2a00e133
“Jayce” <15332019087@163.com>
2023-03-27 22:44:06 +0800

    better-scroll封装

---
+++ src/components/common/swiper/ElementSwiper.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="block">
+    <el-carousel height="195px">
+      <el-carousel-item v-for="(item, index) in banner" :key="index">
+        <!-- <h3 class="small">{{ item }}</h3> -->
+        <img :src="item.image" alt="" class="small" style="width: 100%" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+<script>
+export default {
+  name: "ElementSwiper",
+  props: {
+    banner: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+<style>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,67 +1,70 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banner="banners"></home-swiper>
-    <recommends-view :recommend="recommends"></recommends-view>
-    <feature-view></feature-view>
-    <tab-control
-      @tabClick="homeTabClick"
-      :title="['流行', '新款', '精选']"
-      class="tab-control"
-    ></tab-control>
-    <goods-list :goodsList="goods[currentType].list"></goods-list>
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-    </ul>
+    <scroll class="content">
+      <!-- <home-swiper :banner="banners"></home-swiper> -->
+      <element-swiper :banner="banners"></element-swiper>
+      <recommends-view :recommend="recommends"></recommends-view>
+      <feature-view></feature-view>
+      <tab-control
+        @tabClick="homeTabClick"
+        :title="['流行', '新款', '精选']"
+        class="tab-control"
+      ></tab-control>
+      <goods-list :goodsList="goods[currentType].list"></goods-list>
+      <ul>
+        <li>列表1</li>
+        <li>列表2</li>
+        <li>列表3</li>
+        <li>列表4</li>
+        <li>列表5</li>
+        <li>列表6</li>
+        <li>列表7</li>
+        <li>列表8</li>
+        <li>列表9</li>
+        <li>列表10</li>
+        <li>列表11</li>
+        <li>列表12</li>
+        <li>列表13</li>
+        <li>列表14</li>
+        <li>列表15</li>
+        <li>列表16</li>
+        <li>列表17</li>
+        <li>列表18</li>
+        <li>列表19</li>
+        <li>列表20</li>
+        <li>列表21</li>
+        <li>列表22</li>
+        <li>列表23</li>
+        <li>列表24</li>
+        <li>列表25</li>
+        <li>列表26</li>
+        <li>列表27</li>
+        <li>列表28</li>
+        <li>列表29</li>
+        <li>列表30</li>
+        <li>列表31</li>
+        <li>列表32</li>
+        <li>列表33</li>
+        <li>列表34</li>
+        <li>列表35</li>
+        <li>列表36</li>
+        <li>列表37</li>
+        <li>列表38</li>
+        <li>列表39</li>
+        <li>列表40</li>
+        <li>列表41</li>
+        <li>列表42</li>
+        <li>列表43</li>
+        <li>列表44</li>
+        <li>列表45</li>
+        <li>列表46</li>
+        <li>列表47</li>
+        <li>列表48</li>
+        <li>列表49</li>
+        <li>列表50</li>
+      </ul>
+    </scroll>
   </div>
 </template>
 
@@ -69,8 +72,10 @@
 import NavBar from "components/common/navBar/NavBar.vue";
 import TabControl from "components/context/tabControl/TabControl.vue";
 import GoodsList from "components/context/goods/GoodsList.vue";
+import Scroll from "components/common/scroll/Scroll.vue";
 
-import HomeSwiper from "./childComp/HomeSwiper.vue";
+// import HomeSwiper from "./childComp/HomeSwiper.vue";
+import ElementSwiper from "components/common/swiper/ElementSwiper.vue";
 import RecommendsView from "./childComp/RecommendsView.vue";
 import FeatureView from "./childComp/FeatureView.vue";
 
@@ -79,13 +84,16 @@ import { getHomeMultiData, getHomeData } from "network/home";
 export default {
   components: {
     NavBar,
-    HomeSwiper,
+    // HomeSwiper,
+    ElementSwiper,
     RecommendsView,
     FeatureView,
     TabControl,
     GoodsList,
+    Scroll,
   },
   name: "Home",
+
   data() {
     return {
       banners: [],
@@ -144,7 +152,9 @@ export default {
 
 <style scoped>
 #home {
-  padding-top: 44px;
+  height: 100vh;
+  /* padding-top: 44px; */
+  position: relative;
 }
 .home-nav {
   background-color: var(--color-text);
@@ -158,5 +168,14 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
+}
+.content {
+  /* height: 300px; */
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
 }
 </style>
